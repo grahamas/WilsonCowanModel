7e179196b09947ca6d09acbf55ac9fe0916c33fa
Graham Smith <grahamas@gmail.com>
2019-05-19 13:00:03 -0500

    [wip] in process of abstracting common components

--- src/WilsonCowanModel.jl
+++ src/WilsonCowanModel.jl
@@ -7,19 +7,14 @@ import Simulation73: target_loss
 using Random
 using MacroTools, IterTools
 using TensorOperations
+using BioNeuralNetworkModels
 
 export WCMSpatial
 
-export AbstractNonlinearity, SigmoidNonlinearity, Sech2Nonlinearity, GaussianNonlinearity
-
 export AbstractStimulus, SharpBumpStimulus, NoisyStimulus, GaussianNoiseStimulus, NoStimulus
 
-export AbstractConnectivity, ShollConnectivity, MeijerConnectivity, GaussianConnectivity
-
 include("helpers.jl")
-include("nonlinearity.jl")
 include("stimulus.jl")
-include("connectivity.jl")
 include("models.jl")
 
 # using Optim
